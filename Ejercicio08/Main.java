300e7560c7fbcd525939e323c7ceaee7f3a1bd7c
JulioTerán <julio.teran.90@hotmail.com>
2026-09-25 00:18:03 -0500

    Add Ejercicio8_DetectorErrores class with main method
    compilacion

---
+++ Ejercicio08/Main.java
@@ -0,0 +1,16 @@
+public class Ejercicio8_DetectorErrores {
+
+    public static void main(String[] args) {
+
+        int numero = 1;
+
+        while (numero < 10) {
+
+            System.out.println(numero);
+
+            numero++;
+        }
+
+        System.out.println("Ciclo finalizado correctamente.");
+    }
+}
